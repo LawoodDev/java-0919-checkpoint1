4ceeeeb31e1b6b9446bf68608eab42461be351bf
LawoodDev <erwann.sale96@gmail.com>
2019-10-25 11:09:39 +0200

    Finish work:Java class and done quiz

--- algos/Student.java
+++ algos/Student.java
@@ -1,10 +1,37 @@
 public class Student {
 
-    // TODO hasLegalAge
+    /**
+     * Return if age is up or equals to 18
+     * @param age The age to test
+     * @return
+     */
+    public static boolean hasLegalAge(int age){
+        return age>= 18;
+    }
 
-    // TODO getGroup
+    /**
+     * Return if student is from group A or B
+     * @param studentId
+     * @return
+     */
+    public static char getGroup(int studentId){
+        return studentId%2==0? 'A':'B';
+    }
 
-    // TODO countStudents
+    /**
+     * Count java students from list
+     * @param students All the languages choose
+     * @return The number of java occurences
+     */
+    public static int countStudents(String[] students){
+        int count=0;
+        for(String s : students)
+        {
+            if (s.toLowerCase().equals("java"))
+                count++;
+        }
+        return count;
+    }
 }
 
 
